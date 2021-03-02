77b7a62c139a724367a2f2402cf3a3c939c344b8
Siebe Corstjens <Siebe.Corstjens@realdolmen.com>
2021-03-02 14:01:50 +0100

    Save changes

---
+++ backend/src/RMotownFestival.Api/DAL/MotownDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RMotownFestival.Api.Domain;
+
+namespace RMotownFestival.Api.DAL
+{
+    public class MotownDbContext : DbContext
+    {
+        public MotownDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Stage> Stages { get; set; }
+    }
+}
